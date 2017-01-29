0013075480e054c5bed91bc65a4925557df1eb9c
fedemiliano <federico.m.gascon@gmail.com>
2017-01-29 15:35:51 +0100

    Changes in firebase config

--- src/config.dev.js
+++ src/config.dev.js
@@ -1,5 +1,5 @@
 export const firebaseConfig = {     
-    apiKey: "",
+    apiKey: "AIzaSyDbXlGyo219Bn3HH2qvb4E7To9GS8OB8-M",
     authDomain: "react-app-55397.firebaseapp.com",
     databaseURL: "https://react-app-55397.firebaseio.com",
     storageBucket: "react-app-55397.appspot.com"
--- src/config.prod.js
+++ src/config.prod.js
@@ -1,5 +1,5 @@
 export const firebaseConfig = {     
-    apiKey: "",
+    apiKey: "AIzaSyA403WV1SB1EV50iwreJPg2V_A911YpdcU",
     authDomain: "learning-react-spa.firebaseapp.com",
     databaseURL: "https://learning-react-spa.firebaseio.com",
     storageBucket: "learning-react-spa.appspot.com"
